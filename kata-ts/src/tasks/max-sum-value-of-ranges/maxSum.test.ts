cf231526a4ed5d9164fa16493a16aae5304a43ee
Seva Kraynov <seva.kraynov@gmail.com>
2021-08-03 16:42:16 +0400

    task: задача на поиск максимума суммы значений интервалов (#8)

---
+++ kata-ts/src/tasks/max-sum-value-of-ranges/maxSum.test.ts
@@ -0,0 +1,15 @@
+import maxSum from "./maxSum";
+
+describe("the maximum sum value of ranges -- challenge version", () => {
+  test("maximum is 6", () => {
+    expect(maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3],[0,4],[6,8]])).toBe(6);
+  });
+
+  test("maximum is 5", () => {
+    expect(maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3]])).toBe(5);
+  });
+
+  test("maximum is 0", () => {
+    expect(maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,4],[2,5]])).toBe(0);
+  });
+});
