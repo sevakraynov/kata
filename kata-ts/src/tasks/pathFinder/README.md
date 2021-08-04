09aba7f7d84048544c5527dde3f486666953f092
sevakraynov <seva.kraynov@gmail.com>
2021-08-04 17:54:17 +0400

    task: поиск кратчайшего пути

--- kata-ts/src/tasks/pathFinder/README.md
+++ kata-ts/src/tasks/pathFinder/README.md
@@ -7,3 +7,11 @@ You are at position `[0, 0]` in maze `NxN` and you can **only** move in one of t
 * Empty positions are marked `.`;
 * Walls are marked `W`;
 * Start and exit positions are empty in all test cases.
+
+## [Path Finder Series #2: shortest path](https://www.codewars.com/kata/57658bfa28ed87ecfa00058a) - Условие
+
+You are at position `[0, 0]` in maze `NxN` and you can **only** move in one of the four cardinal directions (i.e. North, East, South, West). Return the minimal number of steps to exit position `[N-1, N-1]` if it is possible to reach the exit from the starting position. Otherwise, return `false`.
+
+* Empty positions are marked `.`;
+* Walls are marked `W`;
+* Start and exit positions are empty in all test cases.
--- kata-ts/src/tasks/pathFinder/pathFinder.test.ts
+++ kata-ts/src/tasks/pathFinder/pathFinder.test.ts
@@ -1,4 +1,4 @@
-import { reachTheExit } from "./pathFinder";
+import { reachTheExit, shortestPath } from "./pathFinder";
 
 describe("path finder #1: can you reach the exit?", () => {
 
@@ -41,5 +41,49 @@ W..`;
 ....W.`;
 
     expect(reachTheExit(maze)).toBeFalsy();
+  });
+});
+
+describe("path finder #2: shortest path", () => {
+
+  test("basic test #1", () => {
+    const maze =
+`.W.
+.W.
+...`;
+    expect(shortestPath(maze)).toBe(4);
+  });
+
+  test("basic test #2", () => {
+    const maze =
+`.W.
+.W.
+W..`;
+
+    expect(shortestPath(maze)).toBeFalsy();
+  });
+
+  test("basic test #3", () => {
+    const maze =
+`......
+......
+......
+......
+......
+......`;
+
+    expect(shortestPath(maze)).toBe(10);
+  });
+    
+  test("basic test #4", () => {
+    const maze =
+`......
+......
+......
+......
+.....W
+....W.`;
+
+    expect(shortestPath(maze)).toBeFalsy();
   });
 });
