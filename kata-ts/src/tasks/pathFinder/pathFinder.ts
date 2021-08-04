37360791c600ffc36c5c81e74a0e4fd0c013501c
Seva Kraynov <seva.kraynov@gmail.com>
2021-08-04 18:57:20 +0400

    task: поиск кратчайшего пути (#11)

--- kata-ts/src/tasks/pathFinder/pathFinder.ts
+++ kata-ts/src/tasks/pathFinder/pathFinder.ts
@@ -26,4 +26,40 @@ const reachTheExit = (maze: string): boolean => {
   return mazeArray[lastIndex][lastIndex] === "*";
 }
 
-export { reachTheExit };
+const shortestPath = (maze: string): number | boolean => {
+  
+  const mazeArray = maze.split('\n').map(r => r.split(""));
+  const lastIndex = mazeArray.length - 1;
+  const possiblePath = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+  const startX = 0;
+  const startY = 0;
+
+  let cellsToProcess = [{
+    x: startX, y: startY, step: 0
+  }];
+
+  while(cellsToProcess.length) {
+    const {x, y, step} = cellsToProcess.shift()!;
+    if(x === lastIndex && y === lastIndex) {
+      return step;
+    }
+    
+    mazeArray[x][y] = "W";
+
+    for(let i = 0; i < possiblePath.length; i++ ) {
+      const [deltaX, deltaY] = possiblePath[i];
+      const nextX = x + deltaX;
+      const nextY = y + deltaY;
+
+      if(mazeArray[nextX] && mazeArray[nextX][nextY] && mazeArray[nextX][nextY] !== "W") {
+          cellsToProcess.push({x: nextX, y: nextY, step: step + 1});
+          mazeArray[nextX][nextY] = "W"
+      }
+    }
+  }
+
+  return false;
+};
+
+export { reachTheExit, shortestPath };
