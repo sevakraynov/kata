8ec4624829da8849647f9828970e24317ce981e7
sevakraynov <seva.kraynov@gmail.com>
2021-09-20 10:22:54 +0300

    chore: refactor tasks by prettier

--- kata-ts/src/tasks/max-sum-value-of-ranges/maxSum.ts
+++ kata-ts/src/tasks/max-sum-value-of-ranges/maxSum.ts
@@ -1,20 +1,20 @@
 const maxSum = (arr: number[], ranges: [number, number][]) => {
   const sumFrom0ToN = [arr[0]];
 
-  for(let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     sumFrom0ToN[i] = sumFrom0ToN[i - 1] + arr[i];
   }
-  
+
   let currentMaxSum = -Infinity;
-  for(let i = 0; i < ranges.length; i++) {
+  for (let i = 0; i < ranges.length; i++) {
     const [start, end] = ranges[i];
     const sum = sumFrom0ToN[end] - (sumFrom0ToN[start - 1] || 0);
-    if(sum > currentMaxSum) {
+    if (sum > currentMaxSum) {
       currentMaxSum = sum;
     }
   }
 
   return currentMaxSum;
-}
+};
 
 export default maxSum;
